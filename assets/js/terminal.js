41fa6901f2b4d23fb568b0b6d46fb34d91aa3bae
rzabala24-0038-a11y <rzabala24-0038@cca.edu.ph>
2025-08-24 20:55:16 +0800

    Create terminal.js

---
+++ assets/js/terminal.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#terminal", {
+    strings: [
+      "Initializing Terminal...\nDone...\n01010111 01100101 01101100 01100011 01101111 01101101 01100101 00100000\n01110100 01101111 00100000 01101101 01111001 00100000 01110000 01101111\n01110010 01110100 01100110 01101111 01101100 01101001 01101111"
+    ],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: false,
+    showCursor: true,
+    cursorChar: "█",
+    smartBackspace: false
+  });
+});
